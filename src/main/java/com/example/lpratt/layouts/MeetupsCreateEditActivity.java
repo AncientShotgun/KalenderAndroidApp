054edc0fc754b88a963c320b7b461c62c7b44f41
AncientShotgun <tf2maniac>
2016-05-30 13:51:58 +0200

    Improvement of the Activity. (ver. 2)

--- src/main/java/com/example/lpratt/layouts/MeetupsCreateEditActivity.java
+++ src/main/java/com/example/lpratt/layouts/MeetupsCreateEditActivity.java
@@ -2,6 +2,7 @@ package com.example.lpratt.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -17,16 +18,28 @@ public class MeetupsCreateEditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Meetups:");
+
+        if (savedInstanceState.getString("mode").equals("create")) {
+            setTitle("Meetups: Create ");
+        } else if (savedInstanceState.getString("mode").equals("create")) {
+            setTitle("Meetups:");
+        }
+
         setContentView(R.layout.activity_meetups_create_edit);
         ArrayList<String> importanceList = new ArrayList<>();
         importanceList.add("Unimportant");
         importanceList.add("Important");
         importanceList.add("Very Important");
-        ArrayAdapter<String> importanceText = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,importanceList);
+        ArrayAdapter<String> importanceText = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, importanceList);
         importance.setAdapter(importanceText);
 
     }
 
+    public void onClickCollectData(View view) {
 
+        String locText = loc.getText().toString();
+        String dateText = date.getText().toString();
+        String importanceText = (String) importance.getSelectedItem();
+
+    }
 }
